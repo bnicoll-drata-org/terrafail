23b5a05f6ec7f276d3e872bb0dbcbda42b9f0ab4
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2026-03-04 16:27:15 +0000

    Drata Compliance Recommendations

--- aws/_modules/iam/main.tf
+++ aws/_modules/iam/main.tf
@@ -124,6 +124,7 @@ resource "aws_iam_role" "TerraFailIAM_role_inline" {
 }
 
 resource "aws_iam_role" "TerraFailIAM_role_managed" {
+  # Drata: Configure [aws_iam_role.tags] to ensure that organization-wide tagging conventions are followed.
   name = "TerraFailIAM_role_managed"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
